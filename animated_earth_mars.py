0626d0db8f72cafcd09047f72579a069be1eea7e
Gubscruz <124577208+Gubscruz@users.noreply.github.com>
2023-06-15 12:13:13 -0300

    smoother animation

--- animated_earth_mars.py
+++ animated_earth_mars.py
@@ -14,7 +14,7 @@ r2 = 42164e3  # radius of higher circular orbit (Mars' orbit)
 a = (r1 + r2) / 2
 
 # Time array for one full orbit in the transfer ellipse
-T = np.linspace(0, 2*np.pi, 300)
+T = np.linspace(0, 2*np.pi, 1000)
 
 # Position function for an elliptical orbit with semi-major axis a and eccentricity e
 def position(a, e, T):
@@ -34,8 +34,8 @@ ax.add_artist(earth_orbit)
 ax.add_artist(mars_orbit)
 
 # Add planets to plot
-earth_radius = r1/10
-mars_radius = r2/10
+earth_radius = r1/5
+mars_radius = r2/17
 earth = Circle((-r1, 0), earth_radius, color='b')
 mars = Circle((r2, 0), mars_radius, color='r')
 ax.add_artist(earth)
@@ -44,25 +44,38 @@ ax.add_artist(mars)
 # Plot spacecraft
 spacecraft, = ax.plot([], [], 'go')
 
+# Create list to store spacecraft path
+spacecraft_path, = ax.plot([], [], 'g', linestyle='--')
+
 # Initialize function
 def init():
     spacecraft.set_data([], [])
-    return earth, mars, spacecraft,
+    spacecraft_path.set_data([], [])
+    return earth, mars, spacecraft, spacecraft_path,
 
 # Eccentricity of the transfer orbit
 e = 1 - 2 / ((r2 / r1) + 1)
+
+# Lists to store spacecraft path coordinates
+xdata = []
+ydata = []
 
 # Update function
 def update(i):
     x, y = position(a, e, T[i])
     spacecraft.set_data(x, y)
-    
+
+    xdata.append(x)
+    ydata.append(y)
+    spacecraft_path.set_data(xdata, ydata)
+
     earth.center = r1 * np.cos(T[i]), r1 * np.sin(T[i])
     mars.center = r2 * np.cos(T[i]), r2 * np.sin(T[i])
-    
-    return earth, mars, spacecraft,
 
-# Create animation
-ani = FuncAnimation(fig, update, frames=range(len(T)), init_func=init, blit=True)
+    return earth, mars, spacecraft, spacecraft_path,
+
+fig.dpi = 120
+
+ani = FuncAnimation(fig, update, frames=range(len(T)), init_func=init, blit=True, interval=20)
 
 plt.show()
